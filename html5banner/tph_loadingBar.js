d051e169286c93fab8f95a1c678f3758fccc730f
MatthewStone218 <121588111+MatthewStone218@users.noreply.github.com>
2023-02-06 17:00:47 +0900

    Add files via upload

--- html5banner/tph_loadingBar.js
+++ html5banner/tph_loadingBar.js
@@ -2,6 +2,9 @@ window.gml_Script_gmcallback_scaling();
 
 function loadingBar_hook(ctx, width, height, total, current, image)
 {
+	const img = new Image();
+	img.src = 'https://cdn.pixabay.com/photo/2014/06/03/19/38/test-361512_960_720.jpg';
+	
 	var time = window.gml_Script_gmcallback_current_time();
 	var imgWidth = 200;
 	var imgHeight = 200;
@@ -14,7 +17,7 @@ function loadingBar_hook(ctx, width, height, total, current, image)
 	ctx.translate(width*0.5,height*0.5);
 	ctx.rotate((Math.PI/180)*floor(time/80)*30);
 	ctx.translate(-width*0.5,-height*0.5);
-	ctx.drawImage(image,(width/2)-(imgWidth/2),(height/2)-(imgHeight/2),imgWidth,imgHeight);
+	ctx.drawImage(img.src,(width/2)-(imgWidth/2),(height/2)-(imgHeight/2),imgWidth,imgHeight);
 
 	ctx.translate(width*0.5,height*0.5);
 	ctx.rotate(-(Math.PI/180)*floor(time/80)*30);
